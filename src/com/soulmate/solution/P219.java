ad8959168709f762adee39dcd5465758b9bf2e64
stay <further181216@gmail.com>
2022-01-19 16:46:33 +0800

    null

---
+++ src/com/soulmate/solution/P219.java
@@ -0,0 +1,20 @@
+package com.soulmate.solution;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class P219 {
+
+    public static void main(String[] args){
+
+    }
+
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, List<Integer>> hashMap = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+
+        }
+        return false;
+    }
+
+}
